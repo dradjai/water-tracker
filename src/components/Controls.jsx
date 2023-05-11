e7db8f2928a70f737db1a59768e563083e076495
David Radjai <dradjai@yahoo.com>
2023-05-11 16:58:31 -0400

    added buttons to add cut, bottle and reset as well as swipe feature

---
+++ src/components/Controls.jsx
@@ -0,0 +1,24 @@
+import { Box, AddIcon, Text, Button } from "native-base";
+
+export default function Controls({ intake, setIntake }) {
+  return (
+    <Box width="100%" alignItems="center" justifyContent="center">
+      <Text color="whitesmoke" fontSize="xl" mb={4}>
+        <AddIcon color="whitesmoke" /> Add a portion of water
+      </Text>
+      <Box flexDir="row" mb={6} width="80%" justifyContent="space-around">
+        <Button colorScheme="blue" size="lg" borderRadius={16} pl={6} pr={6}
+        onPress={ () => setIntake(intake + 500)}>
+          <Text color="whitesmoke" fontWeight="700" fontSize="lg">💧One Cup</Text>
+        </Button>
+        <Button colorScheme="blue" size="lg" borderRadius={16} pl={6} pr={6}
+        onPress={ () => setIntake(intake + 1000)}>
+          <Text color="whitesmoke" fontWeight="700" fontSize="lg">💦One Bottle</Text>
+        </Button>
+      </Box>
+      <Button colorScheme="gray" pl={8} pr={8} borderRadius={16} onPress={() => setIntake(0)}>
+        <Text color="whitesmoke" fontSize="lg" fontWeight="500">🚫 Reset</Text>
+      </Button>
+    </Box>
+  );
+}
